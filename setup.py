cf0b37e55a86bd976b5aba5172a37b94e921c5c8
Bhagwan Subedi <bhag1subedi@gmail.com>
2020-05-21 00:54:36 +0545

    initial commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='bdistributionpy',
+      version='0.1',
+      description='Gaussian distributions and binomial distribution',
+      packages=['bdistributionpy'],
+      author_name='Bhagawan Subedi',
+      zip_safe=False)
