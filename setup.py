a5cdf4aba79dc97952c0a18a0f0755ab8e17df67
Bhagwan Subedi <bhag1subedi@gmail.com>
2020-05-21 01:00:16 +0545

    add license and readme

--- setup.py
+++ setup.py
@@ -4,5 +4,4 @@ setup(name='bdistributionpy',
       version='0.1',
       description='Gaussian distributions and binomial distribution',
       packages=['bdistributionpy'],
-      author_name='Bhagawan Subedi',
       zip_safe=False)
